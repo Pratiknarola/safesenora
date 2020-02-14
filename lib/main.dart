c86a8e2ba8048a54526d1130699dcfe4d8fbdc43
Pratik Narola <narolapratik11@gmail.com>
2020-02-15 01:11:59 +0530

    removed default code

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
